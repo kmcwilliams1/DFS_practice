73d933e4bb7e98005905536dbf262a63464fc7a0
Kyle McWilliams <KyleMcWilliams2@my.unt.edu>
2024-02-03 18:31:13 -0600

    reading in file complete

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -25,8 +28,8 @@ void dfs(int row, int col) {
     printf("Visiting cell (%d, %d) \n", row, col);
 
 
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
             int newRow = row + i;
             int newCol = col + j;
 
@@ -37,12 +40,42 @@ void dfs(int row, int col) {
 
 int main() {
     // Read input
-    Cell region[*ROWS][*COLS];
+  //  Cell region[*ROWS][*COLS];
+
+
+    ifstream fin("data2.dat");
+    if (!fin.is_open()) {
+        cerr << "Failed to open data.dat" << endl;
+        return 1;
+    }
+    string readingLine, readingPart;
+
+
+    int pos = 1;
+
+    while (getline(fin, readingLine)) {
+        cout << "readingLine: " << readingLine << endl;
+
+        //read each character of the readingLine line
+        for (int i = 0; i < readingLine.length(); i++) {
+            readingLine[i] == ',' && pos++;
+            if (readingLine[i] != ' ' && readingLine[i] != ',')
+            cout << "readingLine[" << i << "]: " << readingLine[i] << " at position " << pos << endl;
+        }
+        pos = 1;
+        cout << endl;
 
 
 
-    // Call DFS from a starting point
-    dfs(0, 0);
+    }
+
+
+    fin.clear();
+    fin.seekg(0, ios::beg);
+
+
+        // Call DFS from a starting point
+ //   dfs(0, 0);
 
 
     return 0;
